b03c966ff94ebe5e5674f13d2d3eb0f1489cb7ad
Tongaston <gaston.moure@gmail.com>
2023-09-15 13:29:05 +0200

    no cursor

--- 03-Mouse follower/src/App.jsx
+++ 03-Mouse follower/src/App.jsx
@@ -1,16 +1,15 @@
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from 'react'
 
 const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
+  // pointer move
   useEffect(() => {
-    console.log('useEffect', { enabled })
+    console.log('effect ', { enabled })
 
     const handleMove = (event) => {
       const { clientX, clientY } = event
-      console.log('handleMove', { clientX, clientY })
       setPosition({ x: clientX, y: clientY })
     }
 
@@ -18,12 +17,20 @@ const FollowMouse = () => {
       window.addEventListener('pointermove', handleMove)
     }
 
-    //cleanup
-    return () => {
+    // cleanup:
+    return () => { 
       console.log('cleanup')
       window.removeEventListener('pointermove', handleMove)
     }
+  }, [enabled])
 
+  // change body className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
   }, [enabled])
 
   return (
@@ -31,14 +38,14 @@ const FollowMouse = () => {
       <div style={{
         position: 'absolute',
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        border: '1px solid white',
+        border: '1px solid #fff',
         borderRadius: '50%',
         opacity: 0.8,
         pointerEvents: 'none',
         left: -25,
         top: -25,
-        width: 40,
-        height: 40,
+        width: 50,
+        height: 50,
         transform: `translate(${position.x}px, ${position.y}px)`
       }}
       />
@@ -49,7 +56,7 @@ const FollowMouse = () => {
   )
 }
 
-function App() {
+function App () {
   return (
     <main>
       <FollowMouse />

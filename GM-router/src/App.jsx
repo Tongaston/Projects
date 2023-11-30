cee3dbd02e8573d09202f0c3448dbbe48ac3b06e
Tongaston <gaston.moure@gmail.com>
2023-11-30 17:45:36 +0100

    testing

--- GM-router/src/App.jsx
+++ GM-router/src/App.jsx
@@ -1,26 +1,36 @@
+import { Suspense, lazy } from 'react';
+
 import './App.css';
 import { Router } from './Router.jsx';
 import { Route } from './Route.jsx';
 
-import HomePage from './pages/Home.jsx';
-import AboutPage from './pages/About.jsx';
-import Page404 from './pages/404.jsx';
-import SearchPage from './pages/Search.jsx';
+const LazyAboutPage = lazy(() => import('./pages/About.jsx'))
+const LazyHomePage = lazy(() => import('./pages/Home.jsx'))
+const LazySearchPage = lazy(() => import('./pages/Search.jsx'))
+const LazyPage404 = lazy(() => import('./pages/404.jsx'))
+
 
 const appRoutes = [
   {
+    path: '/:lang/about',
+    Component: LazyAboutPage
+  },
+
+  {
     path: '/search/:query',
-    Component: SearchPage,
+    Component: LazySearchPage,
   },
 ];
 
 function App() {
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404}>
-        <Route path='/' Component={HomePage} />
-        <Route path='/about' Component={AboutPage} />
+      <Suspense fallback={null}>
+      <Router routes={appRoutes} defaultComponent={LazyPage404}>
+        <Route path='/' Component={LazyHomePage} />
+        <Route path='/about' Component={LazyAboutPage} />
       </Router>
+      </Suspense>
     </main>
   );
 }
--- GM-router/src/pages/About.jsx
+++ GM-router/src/pages/About.jsx
@@ -1,17 +1,35 @@
 import { Link } from '../Link.jsx'
 
- export default function AboutPage() {
+const i18n = {
+  es: {
+    title: 'Sobre mi',
+    button: 'Ir a la home',
+    description: 'Hola! Soy GMdev - Creando clon de React Router.' 
+  },
+  en: {
+    title: 'About me',
+    button: 'Go to Home',
+    description: "Hi! I'm GMdev - Creating a clone of React Router."
+  }
+}
+
+const useI18n = (lang) => {
+  return i18n[lang] || i18n.en
+}
+
+ export default function AboutPage({ routeParams }) {
+  const i18n = useI18n(routeParams.lang ?? 'es')
     return (
       <>
-        <h1>About me</h1>
+        <h1>{i18n.title}</h1>
         <div>
           <img
             src='https://media.licdn.com/dms/image/C4D03AQFvIz2B_bhbSg/profile-displayphoto-shrink_800_800/0/1518354907514?e=1706140800&v=beta&t=rbjcWBvIS0GRd3t7Rz3zQPiSExfgmAYeKgF3fNWJQsc'
             alt='GMdev'
           />
-          <p> Hola! Soy GMdev - Creando clon de React Router</p>
+          <p>{i18n.description}</p>
         </div>
-        <Link to='/'>Ir a Home Page</Link>
+        <Link to='/'>{i18n.button}</Link>
       </>
     );
   }
